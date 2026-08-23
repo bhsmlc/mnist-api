11bdc7c2640ee2ff3876d09a118e01ffc806c75a
Ash_ioL <ashwathn.arasurajsankar@gmail.com>
2026-08-22 17:39:25 -0700

    trained model on training data that matches the preprocessed images

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import tensorflow as tf
 from tensorflow.keras import models, layers
 import matplotlib.pyplot as plt
+import cv2
+import numpy as np
 
 data = tf.keras.datasets.mnist
 dataset = data.load_data()
@@ -9,19 +11,68 @@ testing = dataset[0]
 x_train, y_train = training
 x_test, y_test = testing
 
-x_train = 255 - x_train
-x_test = 255 - x_train
+x_train_copy = x_train.copy()
 
-x_train, x_test = x_train / 255.0, x_test / 255.0
+for i in range(60000):
+    print(i)
+    print("\033c")
+    img = x_train[i]
+
+    resized_img = cv2.resize(img, (280, 280), interpolation=cv2.INTER_AREA)
+    _, thresholded_img = cv2.threshold(
+        resized_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU
+    )
+
+    x, y, w, h = cv2.boundingRect(thresholded_img)
+
+    if w == 0 or h == 0:
+        print("weird one")
+        x_train[i] = (img / 255.0).reshape(28, 28)
+        continue
+
+    digit_crop = thresholded_img[y:y+h, x:x+w]
+
+    if w > h:
+        w = h
+        digit_crop = cv2.resize(digit_crop, (w, h))
+
+    scalar = 200 / h
+    new_h = h * scalar
+    new_w = w * scalar
+    new_x = (280-new_w) // 2
+    new_y = (280-new_h) // 2
+
+    digit_crop = cv2.resize(
+        digit_crop,
+        (int(new_w), int(new_h)),
+        interpolation=cv2.INTER_AREA
+    )
+
+    blank = np.zeros((280, 280), dtype=np.uint8)
+
+    blank[
+        int(new_y):int(new_y)+int(new_h),
+        int(new_x):int(new_x)+int(new_w)
+    ] = digit_crop
+
+    resized_img = cv2.resize(blank, (28, 28), interpolation=cv2.INTER_AREA)
+
+    x_train[i] = (resized_img / 255.0).reshape(28, 28)
+
+
+# x_train = 255 - x_train
+# x_test = 255 - x_train
+
+# x_train, x_test = x_train / 255.0, x_test / 255.0
 
 x_train = x_train.reshape(-1, 28, 28, 1)
 x_test = x_test.reshape(-1, 28, 28, 1)
 
-x_train[x_train >= 0.5] = 1
-x_train[x_train < 0.5] = 0
-
 for i in range(10):
     plt.imshow(x_train[i], cmap="gray")
+    plt.title(y_train[i])
+    plt.show()
+    plt.imshow(x_train_copy[i], cmap="gray")
     plt.title(y_train[i])
     plt.show()
 
