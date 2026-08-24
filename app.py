07a087e0623124eff324f60e3bfeb056ba478581
Ash_ioL <ashwathn.arasurajsankar@gmail.com>
2026-08-23 22:19:58 -0700

    added some cross origin allowing thing

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 import cv2
 import numpy as np
@@ -8,6 +9,15 @@ import base64
 from typing import Optional
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 model = models.load_model("mnistmodel.keras")
 
 @app.get("/")
