896e8c11dc5bc5945ea6730939eedeff49e0634d
Ash_ioL <ashwathn.arasurajsankar@gmail.com>
2026-09-01 22:40:51 -0700

    threshold was too high

--- app.py
+++ app.py
@@ -52,10 +52,8 @@ def predict_frame(image: UploadFile = File(...)):
 
     gray_img = 255 - gray_img
     low_val, _ = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-    high_val = min(250, low_val * 2)
+    high_val = min(250, low_val * 1.5)
     _, thresh = cv2.threshold(gray_img, high_val, 255, cv2.THRESH_BINARY)
-    print(f"low_val: {low_val}, high_val: {high_val}")
-    print(f"image: {gray_img}")
 
     #_, thresholded_img = cv2.threshold(gray_img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
     thresholded_img = thresh.copy()
