c4dbda8ee8e8c540e842e90a6c7158b7e6817ebf
Ash_ioL <ashwathn.arasurajsankar@gmail.com>
2026-09-09 23:57:36 -0700

    updated cors

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "https://bhsmlc.github.io"],
+    allow_origins=["http://127.0.0.1:5500", "https://bhsmlc.github.io", "https://bhsmachinelearning.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
